2f74203a5f65b77b03ca7861331af03108cf3149
ZandorZ <sancwb@gmail.com>
2020-12-14 15:48:59 -0300

    show user address

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { Web3Service } from '../services/web3.service';
 
 @Component({
     selector: 'app-layout',
     templateUrl: './layout.component.html',
     styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
 
+    account: string;
 
     isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
         .pipe(
@@ -17,6 +19,13 @@ export class LayoutComponent {
             shareReplay()
         );
 
-    constructor(private breakpointObserver: BreakpointObserver) { }
+    constructor(private breakpointObserver: BreakpointObserver, private web3Serv: Web3Service) {
+
+    }
+    async ngOnInit() {
+        this.account = await this.web3Serv.getAccount();
+    }
+
+
 
 }
