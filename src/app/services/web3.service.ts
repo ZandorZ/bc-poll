841f4019ce1ebedc41ac604cc58b7f0c4a6a4f8b
ZandorZ <sancwb@gmail.com>
2020-12-14 10:46:42 -0300

    Web 3 implementation

---
+++ src/app/services/web3.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import Web3 from 'web3';
+import { Contract } from 'web3-eth-contract';
+
+declare var window: any;
+const contractAbi = require("../../../ethereum/build/contracts/PollContract.json").abi;
+
+@Injectable({
+    providedIn: 'root'
+})
+export class Web3Service {
+
+    private web3: Web3;
+    private contract: Contract;
+    private contractAddress = "0x6835B8F72EDb1f4a1242C07de236764C95a086Aa";
+
+    constructor() {
+        if (window.web3) {
+            this.web3 = new Web3(window.ethereum);
+            this.contract = new this.web3.eth.Contract(contractAbi, this.contractAddress);
+
+            window.ethereum.enable().catch((err: any) => console.error(err));
+        } else {
+            console.error("Metamask not installed.");
+        }
+    }
+
+    async getAccount(): Promise<string> {
+        return this.web3.eth.getAccounts().then(accounts => accounts[0] || '');
+    }
+
+    async executeTransaction(fnName: string, ...args: any[]): Promise<void> {
+        const acc = await this.getAccount();
+        this.contract.methods[fnName](...args).send({ from: acc });
+    }
+
+    async call(fnName: string, ...args: any[]): Promise<any> {
+        const acc = await this.getAccount();
+        try {
+            const data = this.contract.methods[fnName](...args).call({ from: acc });
+            return Promise.resolve(data);
+        } catch(e ) {
+            console.error('error in calling ...', fnName);
+        }
+    }
+
+
+}
